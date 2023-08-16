3d39942bfafc942a7f69d0e9df7ac4795df1ef4d
ormenachem <orchickul@gmail.com>
2023-08-16 23:25:47 +0300

    well im going to sleep no major changes

---
+++ Assets/scripts/player/statManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class statManager : MonoBehaviour
+{
+    public static float health;
+    public static float maxHealth;
+
+    public static bool hasWallClibing;
+    public static bool hasGliding;
+    public static bool hasDoubleJumping;
+    public static bool hasFireBreathing;
+
+    
+}
