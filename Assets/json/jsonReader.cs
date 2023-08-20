063a1de23bc059f386f66fd444d0d254ec4bb94e
ormenachem <orchickul@gmail.com>
2023-08-20 13:39:56 +0300

    json reader fully optamized
    added a class that has a method to read the json file as an object of that class and a method to update the json file with data from an object of this class

--- Assets/json/jsonReader.cs
+++ Assets/json/jsonReader.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class jsonReader : MonoBehaviour
 {
     [SerializeField]private TextAsset statsJSON;
-    public PlayerStats playerStats;
-
-    void Awake(){
-        playerStats = JsonUtility.FromJson<PlayerStats>(statsJSON.text);
-    }
+    
     [System.Serializable]
     public class PlayerStats{
         public float currentHealth;
         public float maxHealth;
-        public bool hasDoubleJump;
+        public bool hasWallJump;
+
+        public PlayerStats getFromJSON(TextAsset jsonFile){
+            return JsonUtility.FromJson<PlayerStats>(jsonFile.text);
+        }
+        public void updateJSON(TextAsset jsonFile){
+            string jsonForm = JsonUtility.ToJson(this, true);
+            File.WriteAllText(AssetDatabase.GetAssetPath(jsonFile), jsonForm);  
+        }
     }
 
 }
